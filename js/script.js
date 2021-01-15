0667b8c3c8a6ceabb17a4b1c0b8b04b19e925765
Daniel <danielpellegrini87@gmail.com>
2021-01-15 10:16:48 +0100

    COMPLETED
    without .find because I didn't use ul>li

--- js/script.js
+++ js/script.js
@@ -2,16 +2,29 @@ $(document).ready(function() {
 
   $('.features > a:first-child').click(function(event) {
     $('.dropdown-container').toggle();
-    $(this).hover(function(event) {
+    $('.dropdown-container').mouseleave(function(event) {
       $('.dropdown-container').toggle();
     });
   });
 
   $('.plans > a:first-child').click(function(event) {
     $('.dropdown-container-small').toggle();
-    $(this).hover(function(event) {
+    $('.dropdown-container-small').mouseleave(function(event) {
       $('.dropdown-container-small').toggle();
     });
   });
 
+  $('.about > a:first-child').click(function(event) {
+    $('.dropdown-container-about').toggle();
+    $('.dropdown-container-about').mouseleave(function(event) {
+      $('.dropdown-container-about').toggle();
+    });
+  });
+
+  $('.help > a:first-child').click(function(event) {
+    $('.dropdown-container-help').toggle();
+    $('.dropdown-container-help').mouseleave(function(event) {
+      $('.dropdown-container-help').toggle();
+    });
+  });
 });
